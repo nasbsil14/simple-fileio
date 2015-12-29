0f5e81718361ba1eb8583c5e4e4b9b76b4a03315
nasbsil14 <nasbsil14@yahoo.co.jp>
2015-12-29 22:06:40 +0900

    up

--- build.sbt
+++ build.sbt
@@ -1,13 +1,10 @@
-lazy val root = (project in file(".")).
-  settings(
-    name := "SimpleFileIO",
-    version := "1.0",
-    scalaVersion := "2.11.4"
-  )
+name := "SimpleFileIO"
+version := "1.0"
+scalaVersion := "2.11.4"
 
 resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies ++= Seq(
-    "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3-1",
-    "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3-1"
+  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.3",
+  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.3"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,49 +1,76 @@
 import scalax.io._
 import scalax.file.Path
+
 object main {
-    def main(args:Array[String]):Unit = {
-        println("SimpleFileIo START")
+  val dataDirPath = Path().toAbsolute / "data"
 
-        //this.createFile
-        //this.getFileList
-        this.writeText
-        this.readText
+  def main(args: Array[String]): Unit = {
+    println("SimpleFileIo START")
+    println(dataDirPath)
 
-        println("SimpleFileIo END")
-    }
+//    this.createFile
+//    this.fileList
+//    this.writeText
+    this.readText
 
-    //ファイル作成
-    def createFile():Unit = {
-        val dirPath:Path = Path("""D:/workspace/SimpleFileIO/data/import""",'/')
-        val filePath:Path = Path("imp_test.txt")
-        val createPath:Path = dirPath / filePath
-        createPath.createFile()
-    }
+    println("SimpleFileIo END")
+  }
 
-    //ファイル一覧取得
-    def getFileList():Unit = {
-        import scalax.file.PathSet
-        val targetDirPath:Path = Path("""D:/workspace/SimpleFileIO/data/import""",'/')
-        val children:PathSet[Path] = targetDirPath.children()
-        children.foreach (child => println(child.path))
-    }
+  //ファイル作成
+  def createFile(): Unit = {
+    val createPath: Path = dataDirPath / "import" / "new_file.txt"
+    createPath.createFile()
+  }
 
-    //ファイル削除
-    //ファイルリネーム
-    //ファイルコピー
-    //ファイル移動
-    //ファイル読み込み（テキスト）
-    def readText():Unit = {
-        val impFile:Input = Resource.fromFile("""D:/workspace/SimpleFileIO/data/import/imp_test.txt""")
-        println(impFile.string(Codec.UTF8))
-    }
-    //ファイル読み込み（xml）
-    //ファイル読み込み（csv）
-    //ファイル書き出し（テキスト）
-    def writeText():Unit = {
-        val expFile:Output = Resource.fromFile("""D:/workspace/SimpleFileIO/data/import/imp_test.txt""")
-        expFile.writeStrings(List("hello","world")," ")(Codec.UTF8)
-    }
-    //ファイル書き出し（xml）
-    //ファイル書き出し（csv）
+  //ファイル一覧取得
+  def fileList(): Unit = {
+    import scalax.file.PathSet
+    val targetDirPath: Path = dataDirPath / "import"
+    val children: PathSet[Path] = targetDirPath.children()
+    children.foreach(child => println(child.path))
+  }
+
+  //ファイル削除
+  def deleteFile(): Unit = {
+    val targetPath: Path = dataDirPath / "import" / "imp_test.txt"
+    targetPath.deleteIfExists()
+  }
+
+  //ファイルリネーム
+  def renameFile(): Unit = {
+    val beforPath: Path = dataDirPath / "import" / "imp_test.txt"
+    val afterPath: Path = dataDirPath / "import" / "imp_test_rename.txt"
+    beforPath.moveTo(afterPath)
+  }
+
+  //ファイルコピー
+  def copyFile(): Unit = {
+    val targetPath: Path = dataDirPath / "import" / "imp_test.txt"
+    val copyPath: Path = dataDirPath / "import" / "imp_test_copy.txt"
+    targetPath.copyTo(copyPath)
+  }
+
+  //ファイル移動
+  def moveFile(): Unit = {
+    val beforPath: Path = dataDirPath / "import" / "imp_test.txt"
+    val afterPath: Path = dataDirPath / "import" / "import2" / "imp_test.txt"
+    beforPath.moveTo(afterPath)
+  }
+
+  //ファイル読み込み（テキスト）
+  def readText(): Unit = {
+    val impFile = dataDirPath / "import" / "imp_test.txt"
+    println(impFile.string(Codec.UTF8))
+  }
+
+  //ファイル読み込み（xml）
+  //ファイル読み込み（csv）
+  //ファイル書き出し（テキスト）
+  def writeText(): Unit = {
+    val expFile: Output = dataDirPath / "import" / "imp_test.txt"
+    expFile.writeStrings(List("hello", "world"), " ")(Codec.UTF8)
+  }
+
+  //ファイル書き出し（xml）
+  //ファイル書き出し（csv）
 }
